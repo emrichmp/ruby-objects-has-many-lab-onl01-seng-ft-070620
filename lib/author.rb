00c7a63906806ccb9a330336b0197b7b18c12411
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-25 21:00:38 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -5,14 +5,14 @@ class Author
     @name = name
   end
   
-  def add_post(post)
+  def add_post(a_post)
     post.author = self
     @@post_count += 1
   end
   
-  def add_post_by_title(post)
-    post = new.Post(post, self)
-    add_post(post)
+  def add_post_by_title(a_post)
+    post = new.Post(a_post, self)
+    add_post(a_post)
     @@post_count += 1
   end
   
