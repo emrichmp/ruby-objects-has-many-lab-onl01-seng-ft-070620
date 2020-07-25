49e57a449bc16ddbb1080f5cdefe046921d3f91d
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-25 21:05:04 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,28 @@
+class Author
+  attr_accessor :name
+  @@post_count = 0
+  def initialize(name)
+    @name = name
+  end
+  
+  def posts
+    Post.all.select do |posts|
+      posts.author == self
+    end
+  end
+  
+  def add_post(posts)
+    posts.author = self
+    @@post_count += 1
+  end
+  
+  def add_post_by_title(posts)
+    posts = Post.new(posts, self)
+    add_post(posts)
+    @@post_count += 1
+  end
+  
+  def self.post_count()
+    @@post_count
+  end
+end
