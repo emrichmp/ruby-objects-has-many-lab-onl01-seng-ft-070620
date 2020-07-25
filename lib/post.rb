a943af3a95dcf7d831a4c125cd62258ccd127ca8
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-25 20:53:13 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,13 @@
 class Post
-  
   attr_accessor :title, :author
   @@all = []
+  
   def initialize(title, aurthor = nil)
     @title = title
     @author = author
     @@all << self
   end
+  
+  
   
 end
